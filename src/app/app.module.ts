edca7c63bee07591dd9d6022d35304eac7a3f524
thayllaa <100003483+thayllaa@users.noreply.github.com>
2023-04-05 20:00:10 -0300

    1 file modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { JurosCompostosComponent } from './components/juros-compostos/juros-compostos.component';
 import { JurosSimplesComponent } from './components/juros-simples/juros-simples.component';
+import { HelloComponent } from './hello.component';
 
 @NgModule({
   imports: [ CommonModule, BrowserModule, FormsModule ],
-  declarations: [ AppComponent, JurosCompostosComponent, JurosSimplesComponent ],
+  declarations: [ AppComponent, HelloComponent, JurosCompostosComponent, JurosSimplesComponent ],
   bootstrap: [ AppComponent ],
 })
 export class AppModule {}
